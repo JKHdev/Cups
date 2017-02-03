6410836e7ea96709bd8a3092e8c492e7fe4b8a15
JKH <kodjus@openmailbox.org>
2017-02-03 15:16:39 +0100

    new files

--- app/src/main/java/pl/javaknowhow/cups/MainActivity.java
+++ app/src/main/java/pl/javaknowhow/cups/MainActivity.java
@@ -1,13 +1,130 @@
 package pl.javaknowhow.cups;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView left, middle, right;
+    List<Integer> cards;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        left = (ImageView) findViewById(R.id.left);
+        middle = (ImageView) findViewById(R.id.middle);
+        right = (ImageView) findViewById(R.id.right);
+
+        cards = new ArrayList<>();
+        cards.add(107); // six
+        cards.add(207); // nine
+        cards.add(407); // A
+
+        Collections.shuffle(cards);
+
+        left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //assign card
+                if(cards.get(0) == 107) {
+                    left.setImageResource(R.drawable.aclub);
+                    Toast.makeText(MainActivity.this, "Guessed!", Toast.LENGTH_SHORT).show();
+                } else if(cards.get(0) == 207) {
+                    left.setImageResource(R.drawable.six);
+                } else if(cards.get(0) == 407) {
+                    left.setImageResource(R.drawable.nine);
+                }
+
+                if(cards.get(1) == 107) {
+                    middle.setImageResource(R.drawable.aclub);
+                } else if(cards.get(1) == 207) {
+                    middle.setImageResource(R.drawable.six);
+                } else if(cards.get(1) == 407) {
+                    middle.setImageResource(R.drawable.nine);
+                }
+
+                if(cards.get(2) == 107) {
+                    right.setImageResource(R.drawable.aclub);
+                } else if(cards.get(2) == 207) {
+                    right.setImageResource(R.drawable.six);
+                } else if(cards.get(2) == 407) {
+                    right.setImageResource(R.drawable.nine);
+                }
+
+            }
+        });
+
+        middle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //assign card
+                if(cards.get(1) == 107) {
+                    middle.setImageResource(R.drawable.aclub);
+                    Toast.makeText(MainActivity.this, "Guessed!", Toast.LENGTH_SHORT).show();
+                } else if(cards.get(1) == 207) {
+                    middle.setImageResource(R.drawable.six);
+                } else if(cards.get(1) == 407) {
+                    middle.setImageResource(R.drawable.nine);
+                }
+
+                if(cards.get(0) == 107) {
+                    left.setImageResource(R.drawable.aclub);
+                } else if(cards.get(0) == 207) {
+                    left.setImageResource(R.drawable.six);
+                } else if(cards.get(0) == 407) {
+                    left.setImageResource(R.drawable.nine);
+                }
+
+                if(cards.get(2) == 107) {
+                    right.setImageResource(R.drawable.aclub);
+                } else if(cards.get(2) == 207) {
+                    right.setImageResource(R.drawable.six);
+                } else if(cards.get(2) == 407) {
+                    right.setImageResource(R.drawable.nine);
+                }
+
+            }
+        });
+
+        right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //assign card
+                if(cards.get(2) == 107) {
+                    right.setImageResource(R.drawable.aclub);
+                    Toast.makeText(MainActivity.this, "Guessed!", Toast.LENGTH_SHORT).show();
+                } else if(cards.get(2) == 207) {
+                    right.setImageResource(R.drawable.six);
+                } else if(cards.get(2) == 407) {
+                    right.setImageResource(R.drawable.nine);
+                }
+
+                if(cards.get(0) == 107) {
+                    left.setImageResource(R.drawable.aclub);
+                } else if(cards.get(0) == 207) {
+                    left.setImageResource(R.drawable.six);
+                } else if(cards.get(0) == 407) {
+                    left.setImageResource(R.drawable.nine);
+                }
+
+                if(cards.get(1) == 107) {
+                    middle.setImageResource(R.drawable.aclub);
+                } else if(cards.get(1) == 207) {
+                    middle.setImageResource(R.drawable.six);
+                } else if(cards.get(1) == 407) {
+                    middle.setImageResource(R.drawable.nine);
+                }
+            }
+        });
     }
 }
